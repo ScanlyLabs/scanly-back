4a3f3aeb5d8d2cc76a1bee32bd41b6f9ab747582
dabin0513 <dabin0513@naver.com>
2026-01-10 16:00:33 +0900

    🎉 프로젝트 생성

---
+++ src/main/java/scanly/io/scanly_back/ScanlyBackApplication.java
@@ -0,0 +1,13 @@
+package scanly.io.scanly_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanlyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScanlyBackApplication.class, args);
+	}
+
+}
