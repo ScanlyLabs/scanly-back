1e99e8b3a20eff369a89c5338ac69cff2664d963
dabin0513 <dabin0513@naver.com>
2026-01-11 16:35:56 +0900

    🔧 개발용 cors 설정

--- src/main/java/scanly/io/scanly_back/common/exception/ErrorCode.java
+++ src/main/java/scanly/io/scanly_back/common/exception/ErrorCode.java
@@ -11,7 +11,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-    DUPLICATE_LOGIN_ID(HttpStatus.CONFLICT, "M003", "이미 사용 중인 로그인 아이디입니다."),
+    DUPLICATE_LOGIN_ID(HttpStatus.CONFLICT, "M003", "이미 사용 중인 로그인 아이디입니다.");
+
     private final HttpStatus httpStatus;
     private final String code;
     private final String message;
