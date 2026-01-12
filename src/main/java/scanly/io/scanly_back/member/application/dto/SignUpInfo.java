24a3e0e7bd1681fa13e929f7bbde7872182d83fd
dabin0513 <dabin0513@naver.com>
2026-01-12 15:00:02 +0900

    ♻️ 회원가입 응답값 수정
    - 회원 아이디도 같이 전달

--- src/main/java/scanly/io/scanly_back/member/application/dto/SignUpInfo.java
+++ src/main/java/scanly/io/scanly_back/member/application/dto/SignUpInfo.java
@@ -3,11 +3,13 @@ package scanly.io.scanly_back.member.application.dto;
 import scanly.io.scanly_back.member.domain.Member;
 
 public record SignUpInfo(
+        String id,
         String loginId
 ) {
 
     public static SignUpInfo from(Member savedMember) {
         return new SignUpInfo(
+                savedMember.getId(),
                 savedMember.getLoginId()
         );
     }
--- src/main/java/scanly/io/scanly_back/member/persentaion/dto/response/SignUpResponse.java
+++ src/main/java/scanly/io/scanly_back/member/persentaion/dto/response/SignUpResponse.java
@@ -3,11 +3,13 @@ package scanly.io.scanly_back.member.persentaion.dto.response;
 import scanly.io.scanly_back.member.application.dto.SignUpInfo;
 
 public record SignUpResponse(
+        String id,
         String loginId
 ) {
 
     public static SignUpResponse from(SignUpInfo signUpInfo) {
         return new SignUpResponse(
+                signUpInfo.id(),
                 signUpInfo.loginId()
         );
     }
