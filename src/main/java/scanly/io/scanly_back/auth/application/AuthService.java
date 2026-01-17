d8ab63581de0611a75507fcb99e7e32454405cb8
dabin0513 <dabin0513@naver.com>
2026-01-17 14:38:05 +0900

    ♻️ 회원 로그인 로직 개선
    - jwt 를 활용하여 로그인하도록 개선

---
+++ src/main/java/scanly/io/scanly_back/auth/application/AuthService.java
@@ -0,0 +1,50 @@
+package scanly.io.scanly_back.auth.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import scanly.io.scanly_back.auth.application.dto.info.LoginInfo;
+import scanly.io.scanly_back.auth.domain.RefreshToken;
+import scanly.io.scanly_back.auth.domain.RefreshTokenRepository;
+import scanly.io.scanly_back.common.exception.CustomException;
+import scanly.io.scanly_back.common.exception.ErrorCode;
+import scanly.io.scanly_back.member.application.MemberService;
+import scanly.io.scanly_back.auth.application.dto.command.LoginCommand;
+import scanly.io.scanly_back.member.domain.Member;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+
+    private final MemberService memberService;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * 로그인 및 토큰 발급
+     * 1. 회원 조회
+     * 2. 비밀번호 검사
+     * 3. access token 및 refresh token 발급
+     * @param command 로그인 정보
+     * @return 로그인 정보
+     */
+    @Transactional
+    public LoginInfo login(LoginCommand command) {
+        // 1. 회원 조회
+        Member member = memberService.findByLoginId(command.loginId());
+
+        // 2. 비밀번호 검사
+        if (!passwordEncoder.matches(command.password(), member.getPassword())) {
+            throw new CustomException(ErrorCode.INVALID_PASSWORD);
+        }
+
+        // 3. access token 및 refresh token 발급
+        String accessToken = jwtTokenProvider.createAccessToken(member.getId());
+        String refreshToken = jwtTokenProvider.createRefreshToken(member.getId());
+
+        return LoginInfo.from(member.getId(), member.getLoginId(), accessToken, refreshToken);
+    }
+}
