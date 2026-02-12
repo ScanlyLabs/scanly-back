3b83bd0b0a2f5d286ab51a5570cd5d0068ad732b
dabin0513 <dabin0513@naver.com>
2026-02-12 17:23:45 +0900

    ♻️ 명함첩 그룹 수정 시 그룹 id null 허용
    - 전체(null) 폴더 변경 허용

--- src/main/java/scanly/io/scanly_back/cardbook/presentation/dto/request/UpdateCardBookGroupRequest.java
+++ src/main/java/scanly/io/scanly_back/cardbook/presentation/dto/request/UpdateCardBookGroupRequest.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 import scanly.io.scanly_back.cardbook.application.dto.command.UpdateCardBookGroupCommand;
 
 public record UpdateCardBookGroupRequest(
-        @NotBlank(message = "그룹 ID는 필수입니다.")
         String groupId
 ) {
     public UpdateCardBookGroupCommand toCommand(String memberId, String id) {
