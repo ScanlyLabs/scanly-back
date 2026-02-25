6864811c6be2eee25d8bacae4ffd82ecfa2b11ba
dabin0513 <dabin0513@naver.com>
2026-02-25 20:39:08 +0900

    ✨ 회원 아이디로 푸시 토큰 삭제

--- src/main/java/scanly/io/scanly_back/notification/domain/PushTokenRepository.java
+++ src/main/java/scanly/io/scanly_back/notification/domain/PushTokenRepository.java
@@ -5,4 +5,5 @@ import java.util.Optional;
 public interface PushTokenRepository {
     PushToken save(PushToken pushToken);
     Optional<PushToken> findByMemberId(String memberId);
+    void deleteByMemberId(String memberId);
 }
